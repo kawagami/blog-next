77cd2aad35b16020dd55fc0c23d92fd85137c958
kawagami <joelai1988@gmail.com>
2024-03-23 16:31:04 +0800

    test: 測試 CICD 功能是否正常

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <header className="min-h-[50px] bg-red-300 dark:bg-purple-300 overflow-hidden hidden sm:grid sm:grid-cols-2 grid-cols-1 grid-rows-1 gap-2">
-        <div className="bg-white dark:bg-black">1231</div>
+        <div className="bg-white dark:bg-black">CICD 測試</div>
         <div className="bg-white dark:bg-black grid grid-cols-5 place-content-center">
           <Link href="/" className="grid place-content-center">link 1</Link>
           <Link href="/" className="grid place-content-center">link 2</Link>
@@ -17,7 +17,9 @@ export default function Home() {
         </div>
       </header>
 
-      <main className="min-h-[calc(100svh-50px-50px)] bg-orange-300 dark:bg-pink-300 overflow-hidden"></main>
+      <main className="min-h-[calc(100svh-50px-50px)] bg-orange-300 dark:bg-pink-300 overflow-hidden">
+        CICD 測試
+      </main>
 
       <footer className="min-h-[50px] bg-yellow-300 dark:bg-teal-300 overflow-hidden"></footer>
     </>
