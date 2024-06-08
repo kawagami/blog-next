583a9e7225a38dcc74710229a3206406b2264548
kawagami <joelai1988@gmail.com>
2024-06-08 10:57:44 +0800

    update: style 調整至較為統一控制

--- components/header.js
+++ components/header.js
@@ -6,8 +6,8 @@ import loglImg from "@/assets/kawagami.svg";
 export default function Header() {
     const iconSize = 50;
     return (
-        <header className="min-h-[50px] bg-white dark:bg-gray-600 dark:text-white overflow-hidden hidden sm:grid sm:grid-cols-2 grid-cols-1 grid-rows-1 gap-2">
-            <div className="bg-white dark:bg-gray-600 flex items-center">
+        <header className="min-h-[50px] overflow-hidden hidden sm:grid sm:grid-cols-2 grid-cols-1 grid-rows-1 gap-2">
+            <div className="flex items-center">
                 <Link href="/">
                     <Image
                         src={loglImg}
@@ -17,7 +17,7 @@ export default function Header() {
                     />
                 </Link>
             </div>
-            <div className="bg-white dark:bg-gray-600 grid grid-cols-5 place-content-center">
+            <div className="grid grid-cols-5 place-content-center">
                 <Link href="/blogs" className="grid place-content-center hover:scale-150 hover:text-blue-400">Blogs</Link>
                 <Link href="/hackmd-notes" className="grid place-content-center hover:scale-150 hover:text-blue-400">HackMD Notes</Link>
                 <Link href="/link3" className="grid place-content-center hover:scale-150 hover:text-blue-400">link 3</Link>
