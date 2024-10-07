3cb06ab9447f2f5bb67cdd5895decbd136fb643a
kawagami <joelai1988@gmail.com>
2024-10-07 20:39:41 +0800

    update: 首頁畫面調整

--- components/index-info.js
+++ components/index-info.js
@@ -2,19 +2,22 @@ import Link from "next/link";
 
 export default function IndexInfo(props) {
     return (
-        <div className={`p-4 border-4 rounded-2xl border-double border-gray-800 dark:border-gray-200${props.href === '/' ? ' col-span-2' : ''}`}>
-            <div className="text-4xl p-4 hover:scale-150">
-                <Link
-                    href={props.href}
-                    className="text-blue-400"
-                >
+        <div className={`p-6 border-4 rounded-2xl border-double border-gray-800 dark:border-gray-200 ${props.href === '/' ? 'col-span-2' : ''} bg-white dark:bg-gray-900 shadow-md transition-transform duration-300`}>
+            <div className="text-4xl font-bold text-blue-500 hover:scale-105 transition-transform">
+                <Link href={props.href} className="hover:text-blue-400">
                     {props.title}
                 </Link>
-                簡介
+                <span className="text-lg text-gray-500 ml-2">簡介</span>
             </div>
-            {
-                props.contents.map((content, index) => <div key={`content_index_${index}`}>{content}</div>)
-            }
+            <div className="mt-4 text-gray-700 dark:text-gray-300">
+                {
+                    props.contents.map((content, index) => (
+                        <div key={`content_index_${index}`} className="mb-2 border-b pb-2 last:border-none">
+                            {content}
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 }
