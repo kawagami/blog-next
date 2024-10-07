687d8774ab485827ab386cb556a3a4de7060a900
kawagami <joelai1988@gmail.com>
2024-10-07 20:28:53 +0800

    update: blog card 畫面調整

--- components/blog-card.js
+++ components/blog-card.js
@@ -4,11 +4,10 @@ export default function BlogCard(props) {
     return (
         <Link
             href={props.href}
-            className="border-4 rounded-xl border-cyan-400 p-2 hover:scale-110 hover:font-bold hover:text-lg hover:text-blue-400"
+            className="border-4 rounded-xl border-cyan-400 p-4 m-2 transition-transform transform hover:scale-105 hover:shadow-lg hover:bg-cyan-50"
         >
-            <div>{props.title}</div>
-            <div>{props.short_content}</div>
-            {/* <div>{props.blog.components.length}</div> */}
+            <div className="text-xl font-semibold mb-2">{props.title}</div>
+            <div className="text-gray-700 mb-4">{props.short_content}</div>
         </Link>
     );
 }
