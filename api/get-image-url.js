c75d1274456991cc1640f958e6b69704f38b36af
kawagami <joelai1988@gmail.com>
2024-10-18 08:00:13 +0800

    update: Remove unnecessary logic from the images page

--- api/get-image-url.js
+++
@@ -1,7 +0,0 @@
-async function getImageUrl() {
-    const res = await fetch(`${process.env.API_URL}`);
-
-    return res.text();
-}
-
-export default getImageUrl;
--- app/images/page.js
+++ app/images/page.js
@@ -1,19 +1,14 @@
 import FirebaseFormComponent from "@/components/firebase-form";
-import getImageUrl from '@/api/get-image-url';
 
 export const metadata = {
-    title: "this is images page",
-    description: "this is images page",
+    title: "Images page",
+    description: "Images page",
 };
 
 export default async function Images() {
-    const imageurl = await getImageUrl();
     return (
         <>
-            {/* <h1>
-                this is images page
-            </h1> */}
-            <FirebaseFormComponent test={imageurl} />
+            <FirebaseFormComponent />
         </>
     )
 }
