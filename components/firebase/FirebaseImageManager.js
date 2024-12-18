8e167befaed9c713684eb9b5a2e1c6e33c6513e4
kawagami <joelai1988@gmail.com>
2024-12-19 00:02:22 +0800

    update: 將 images 展示頁面的 delete 移除

--- components/firebase/FirebaseImageManager.js
+++ components/firebase/FirebaseImageManager.js
@@ -23,7 +23,6 @@ export default function FirebaseImageManager({ initialImages }) {
 
     return (
         <div className="container mx-auto p-6">
-            <h1 className="text-2xl font-semibold mb-4">Firebase Image Manager</h1>
 
             {/* 僅在 /admin/images 時顯示上傳區域 */}
             {pathname === '/admin/images' && (
--- components/firebase/ImageGrid.js
+++ components/firebase/ImageGrid.js
@@ -1,38 +1,47 @@
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
-const ImageGrid = ({ images, deletingImage, copiedImage, onDelete, onCopy }) => (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
-        {images.map((image) => (
-            <div key={image.name} className="bg-white p-4 rounded shadow-md flex flex-col items-center">
-                <Image
-                    width={150}
-                    height={150}
-                    src={image.url}
-                    alt={`Image ${image.name}`}
-                    className="rounded-lg mb-4 object-cover"
-                />
-                <button
-                    onClick={() => onDelete(image.name)}
-                    className={`mt-2 py-1 px-4 rounded transition ${deletingImage === image.name
-                        ? 'bg-gray-500 cursor-not-allowed'
-                        : 'bg-red-500 hover:bg-red-600 text-white'
-                        }`}
-                    disabled={deletingImage === image.name}
-                >
-                    {deletingImage === image.name ? 'Deleting...' : 'Delete'}
-                </button>
-                <button
-                    onClick={() => onCopy(image.url)}
-                    className="mt-2 py-1 px-4 rounded bg-green-500 hover:bg-green-600 text-white"
-                >
-                    Copy URL
-                </button>
-                {copiedImage === image.url && (
-                    <span className="mt-2 text-sm text-green-500">URL 已複製！</span>
-                )}
-            </div>
-        ))}
-    </div>
-);
+const ImageGrid = ({ images, deletingImage, copiedImage, onDelete, onCopy }) => {
+    const pathname = usePathname();
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+            {images.map((image) => (
+                <div key={image.name} className="bg-white p-4 rounded shadow-md flex flex-col items-center">
+                    <Image
+                        width={150}
+                        height={150}
+                        src={image.url}
+                        alt={`Image ${image.name}`}
+                        className="rounded-lg mb-4 object-cover"
+                    />
+                    {/* 僅在 /admin/images 時顯示上傳區域 */}
+                    {pathname === '/admin/images' && (
+                        <button
+                            onClick={() => onDelete(image.name)}
+                            className={`mt-2 py-1 px-4 rounded transition ${deletingImage === image.name
+                                ? 'bg-gray-500 cursor-not-allowed'
+                                : 'bg-red-500 hover:bg-red-600 text-white'
+                                }`}
+                            disabled={deletingImage === image.name}
+                        >
+                            {deletingImage === image.name ? 'Deleting...' : 'Delete'}
+                        </button>
+                    )}
+
+                    <button
+                        onClick={() => onCopy(image.url)}
+                        className="mt-2 py-1 px-4 rounded bg-green-500 hover:bg-green-600 text-white"
+                    >
+                        Copy URL
+                    </button>
+                    {copiedImage === image.url && (
+                        <span className="mt-2 text-sm text-green-500">URL 已複製！</span>
+                    )}
+                </div>
+            ))}
+        </div>
+    )
+};
 
 export default ImageGrid;
