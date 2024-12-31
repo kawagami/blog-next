e33bf0531dae681542176314564b7883f7b0ccd9
kawagami <joelai1988@gmail.com>
2025-01-01 02:38:47 +0800

    update: 更新圖片 URL

--- app/sites/page.js
+++ app/sites/page.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 const data = [
     {
-        src: "https://storage.googleapis.com/fir-test-a67eb.appspot.com/fastapi-upload/5dceb788-cd1e-4555-81ac-7c7bae6dad64.png",
+        src: "https://storage.googleapis.com/fir-test-a67eb.appspot.com/fastapi-upload/c729394e-6133-41c4-bd05-4d37c73ceb5d.png",
         href: "https://kawa.homes",
         alt: "level 2 domain",
         title: "目前的二級域名 kawa.homes",
@@ -19,7 +19,7 @@ const data = [
         ]
     },
     {
-        src: "https://storage.googleapis.com/fir-test-a67eb.appspot.com/fastapi-upload/dcf1c9c8-575c-4460-b1ed-7eb98673403f.png",
+        src: "https://storage.googleapis.com/fir-test-a67eb.appspot.com/fastapi-upload/e42b17c4-d35a-4b51-90d1-98be04489f16.png",
         href: "#",
         alt: "fastapi",
         title: "python firebase service",
@@ -32,7 +32,7 @@ const data = [
         ]
     },
     {
-        src: "https://storage.googleapis.com/fir-test-a67eb.appspot.com/fastapi-upload/a57d4188-b58e-4be2-b43c-7aa45b334bce.png",
+        src: "https://storage.googleapis.com/fir-test-a67eb.appspot.com/fastapi-upload/6fecf05a-0ecb-4943-a767-49b2f42b0dde.png",
         href: "https://axum.kawa.homes",
         alt: "後端 API 站點",
         title: "後端 API 站點",
