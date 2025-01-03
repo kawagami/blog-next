fb553886c283513c517ed66dbf2d45801effccbb
kawagami <joelai1988@gmail.com>
2025-01-04 01:24:25 +0800

    update: 更新 footer 樣式

--- components/footer.js
+++ components/footer.js
@@ -7,27 +7,20 @@ import { useDarkContext } from "@/provider/dark-provider";
 import Link from "next/link";
 
 export default function Footer() {
-    const { isDark, setIsDark } = useDarkContext();
+    const { isDark } = useDarkContext();
     return (
-        <footer className="min-h-[50px] overflow-hidden grid grid-cols-5 gap-4 text-center">
-            <div className="col-start-2 flex justify-center items-center">
-                {/* block 1 */}
-            </div>
+        <footer className="min-h-[50px] flex justify-center gap-4 text-center">
 
-            <div className="flex justify-center items-center">
-                <Link className="hover:scale-150" target="_blank" href="https://github.com/kawagami">
-                    {
-                        isDark
-                            ?
-                            <Image src={GithubMarkWhite} width={30} height={30} alt="my github" />
-                            :
-                            <Image src={GithubMark} width={30} height={30} alt="my github" />
-                    }
-                </Link>
-            </div>
-            <div className="">
-                {/* block 3 */}
-            </div>
+            <Link className="hover:scale-90" target="_blank" href="https://github.com/kawagami">
+                {
+                    isDark
+                        ?
+                        <Image src={GithubMarkWhite} width={30} height={30} alt="my github" />
+                        :
+                        <Image src={GithubMark} width={30} height={30} alt="my github" />
+                }
+            </Link>
+
         </footer>
     );
 }
