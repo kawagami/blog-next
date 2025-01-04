db5c34b8e9de0577409f45351ae9d7afa8945afd
kawagami <joelai1988@gmail.com>
2025-01-04 23:58:05 +0800

    update: 統一使用 app 下的 loading

--- app/admin/images/loading.js
+++
@@ -1,6 +0,0 @@
-import loading from "@/assets/loading.svg"
-import Image from "next/image";
-
-export default function Loading() {
-  return <Image src={loading} width={300} height={300} alt="loading" />;
-}
