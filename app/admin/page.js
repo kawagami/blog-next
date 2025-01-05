eb1ffff02703788369f4aa9317c70afc6dce90fa
kawagami <joelai1988@gmail.com>
2025-01-05 20:28:59 +0800

    update: 調整 admin 版面

--- app/admin/page.js
+++ app/admin/page.js
@@ -6,9 +6,9 @@ export default async function AdminPage() {
     return (
         <>
 
-            <div className="flex flex-col items-center justify-center">
-                <h1 className="text-4xl font-bold text-gray-800 mb-8">Admin Dashboard</h1>
-                <div className="space-y-4">
+            <div className="w-full h-[calc(100svh-180px)] overflow-auto flex flex-col items-center">
+                <h1 className="text-4xl font-bold text-gray-800 my-8">Admin Dashboard</h1>
+                <div className="w-full flex justify-center gap-4">
                     <Link
                         href="/admin/images"
                         className="px-6 py-3 text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 transition duration-300"
