0f90f20ac5bf103c843bb027b68e8a8a5c3c428f
kawagami <joelai1988@gmail.com>
2025-05-30 00:09:37 +0800

    feat: 增加資訊

--- app/admin/stocks/list/page.js
+++ app/admin/stocks/list/page.js
@@ -10,10 +10,17 @@ export default async function List(props) {
 
     const info = await getStockChanges(status);
 
+    const totalChange = info.reduce((sum, item) => {
+        return sum + (item.change ?? 0);
+    }, 0);
+
     return (
         <div className="w-full lg:w-3/5 max-h-[calc(100svh-180px)] overflow-auto p-6 bg-gray-100">
             <h1 className="text-xl font-bold mb-4">
                 股票變動列表 ({info.length} 筆)
+            </h1>
+            <h1 className="text-xl font-bold mb-4">
+                總變動數 {totalChange} %
             </h1>
             <div className="flex gap-2 mb-4">
                 {["", "completed", "failed", "pending"].map((s) => (
