adbccddce3ee11b054d1e4fc6e3ec05c9cab9bf5
kawagami <joelai1988@gmail.com>
2025-06-08 01:06:15 +0800

    feat: 更新開發用的 node 版本

--- app.sh
+++ app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v $PWD:/app -w /app -u $(id -u):$(id -g) node:23.5.0-bookworm $@
+docker run --rm -it -v $PWD:/app -w /app -u $(id -u):$(id -g) node:24-bookworm $@
--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 code . &&
-	docker run --rm -it --name nextjs -v $PWD:/app -w /app -u $(id -u):$(id -g) -p 3000:3000 node:23.5.0-bookworm npm run dev
+	docker run --rm -it --name nextjs -v $PWD:/app -w /app -u $(id -u):$(id -g) -p 3000:3000 node:24-bookworm npm run dev
