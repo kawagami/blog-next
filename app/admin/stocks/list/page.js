77adc14d92bc895b76027dd6362927d01e8b8f09
kawagami <joelai1988@gmail.com>
2025-06-08 00:15:17 +0800

    feat: 增加顯示的資訊

--- app/admin/stocks/list/page.js
+++ app/admin/stocks/list/page.js
@@ -14,13 +14,23 @@ export default async function List(props) {
         return sum + (item.change ?? 0);
     }, 0);
 
+    const totalCount = info.reduce((sum, item) => {
+        return sum + (item.change ? 1 : 0);
+    }, 0);
+
     return (
         <div className="w-full lg:w-3/5 max-h-[calc(100svh-180px)] overflow-auto p-6 bg-gray-100">
             <h1 className="text-xl font-bold mb-4">
-                股票變動列表 ({info.length} 筆)
+                股票變動列表 {info.length} 筆
             </h1>
             <h1 className="text-xl font-bold mb-4">
-                總變動數 {totalChange} %
+                總變動 % 數 {totalChange} %
+            </h1>
+            <h1 className="text-xl font-bold mb-4">
+                有資料的個數 {totalCount}
+            </h1>
+            <h1 className="text-xl font-bold mb-4">
+                平均變動 % 數 {(totalChange / totalCount).toFixed(2)} %
             </h1>
             <div className="flex gap-2 mb-4">
                 {["", "completed", "failed", "pending"].map((s) => (
