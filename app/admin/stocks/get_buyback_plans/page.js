a7fda7ee5213c424c9284bffddfbaace8d5d2034
kawagami <joelai1988@gmail.com>
2025-06-10 00:24:37 +0800

    feat: 實際執行中的庫藏股計畫頁面 todo

---
+++ app/admin/stocks/get_buyback_plans/page.js
@@ -0,0 +1,13 @@
+"use server";
+
+// import { getUnfinishedBuybackPriceGap } from "@/components/stocks/actions";
+
+export default async function Page() {
+    return (
+        <div className="w-full lg:w-3/5 max-h-[calc(100svh-180px)] overflow-auto p-6 bg-gray-100">
+            <h1>
+                我實際執行中的庫藏股計畫頁面
+            </h1>
+        </div>
+    );
+}
