b7c1cc5260f18e271e6803390728beb37e6aa2a6
kawagami <joelai1988@gmail.com>
2025-06-11 21:53:26 +0800

    feat: 實際執行中的庫藏股計畫清單 todo

--- app/admin/stocks/get_buyback_plans/page.js
+++ app/admin/stocks/get_buyback_plans/page.js
@@ -5,8 +5,9 @@
 export default async function Page() {
     return (
         <div className="w-full lg:w-3/5 max-h-[calc(100svh-180px)] overflow-auto p-6 bg-gray-100">
+            {/* 新增 執行中的庫藏股 */}
             <h1>
-                我實際執行中的庫藏股計畫頁面
+                實際執行中的庫藏股計畫清單
             </h1>
         </div>
     );
