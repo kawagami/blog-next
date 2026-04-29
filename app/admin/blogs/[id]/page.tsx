e36ca482e39919c085d7e16a97af28f4a75a9dff
kawagami <joelai1988@gmail.com>
2026-04-29 08:12:10 +0800

    fix: 移除 page 頂部 use server，新增 blog 404 時回傳空白內容

--- app/admin/blogs/[id]/page.tsx
+++ app/admin/blogs/[id]/page.tsx
@@ -1,13 +1,15 @@
-"use server";
-
 import getBlog from "@/api/get-blog";
 import getBlogs from "@/api/get-blogs";
 import BlogComponent from "@/components/blogs/blog-component";
+import type { Blog } from "@/types";
 
 export default async function BlogPage({ params }: { params: Promise<{ id: string }> }) {
     const id = (await params).id;
-    const [blog, blogs] = await Promise.all([getBlog(id), getBlogs()]);
+    const [blogResult, blogs] = await Promise.all([
+        getBlog(id).catch((): Blog => ({ id, markdown: '', tags: [], tocs: [] })),
+        getBlogs(),
+    ]);
     const allTags = Array.from(new Set(blogs.flatMap(item => item.tags))).sort((a, b) => a.localeCompare(b));
 
-    return <BlogComponent id={id} blog={blog} allTags={allTags} />;
+    return <BlogComponent id={id} blog={blogResult} allTags={allTags} />;
 }
