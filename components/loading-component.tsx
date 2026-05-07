d0646001269e0e71cd604430f924bf778275d767
kawagami <joelai1988@gmail.com>
2026-05-07 23:17:38 +0800

    feat(loading): replace spinner with indigo conic-gradient star loader

--- components/loading-component.tsx
+++ components/loading-component.tsx
@@ -1,7 +1,7 @@
 export default function LoadingComponent() {
     return (
         <div className="flex items-center justify-center w-full h-full min-h-40">
-            <div className="w-12 h-12 rounded-full border-4 border-gray-200 dark:border-gray-700 border-t-indigo-500 animate-spin" />
+            <div className="loader" />
         </div>
     );
 }
