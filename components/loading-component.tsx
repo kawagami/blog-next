9536adc8accca143517d7d52a471b7062f637567
kawagami <joelai1988@gmail.com>
2026-05-07 23:09:43 +0800

    refactor(loading): replace SVG spinner with CSS Tailwind spinner

--- components/loading-component.tsx
+++ components/loading-component.tsx
@@ -1,6 +1,7 @@
-import loading from "@/assets/loading.svg";
-import Image from "next/image";
-
 export default function LoadingComponent() {
-    return <Image src={loading} width={300} height={300} alt="loading" />;
+    return (
+        <div className="flex items-center justify-center w-full h-full min-h-40">
+            <div className="w-12 h-12 rounded-full border-4 border-gray-200 dark:border-gray-700 border-t-indigo-500 animate-spin" />
+        </div>
+    );
 }
