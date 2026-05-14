2de8d18a758b074a3c6f356b2ba44c3b2918b9ce
kawagami <joelai1988@gmail.com>
2026-05-15 00:33:18 +0800

    fix: update blog write endpoints to /admin/blogs/:id

--- api/delete-blog.ts
+++ api/delete-blog.ts
@@ -4,7 +4,7 @@ import adminRequest from "@/libs/adminRequest";
 
 async function deleteBlog(id: string): Promise<void> {
     await adminRequest({
-        url: `${process.env.API_URL}/blogs/${id}`,
+        url: `${process.env.API_URL}/admin/blogs/${id}`,
         method: 'DELETE',
     });
 }
--- api/put-blog.ts
+++ api/put-blog.ts
@@ -5,7 +5,7 @@ import type { BlogInput, Blog } from "@/types";
 
 async function putBlog(id: string, blog: BlogInput): Promise<Blog> {
     return adminRequest({
-        url: `${process.env.API_URL}/blogs/${id}`,
+        url: `${process.env.API_URL}/admin/blogs/${id}`,
         headers: { 'Content-Type': 'application/json' },
         method: 'PUT',
         body: JSON.stringify(blog),
