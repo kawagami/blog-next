1d55339470b83079a97b30fd006a5d099e881f98
kawagami <joelai1988@gmail.com>
2026-05-26 22:00:17 +0800

    feat: use next/image in blog preview for automatic WebP and lazy loading

--- components/blogs/blog-preview-component.tsx
+++ components/blogs/blog-preview-component.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
@@ -5,7 +6,22 @@ export default function BlogPreviewComponent({ markdown }: { markdown: string })
     return (
         <div className="w-full h-[calc(100svh-120px)] overflow-auto px-4 py-6">
             <div className="prose prose-gray dark:prose-invert max-w-prose mx-auto">
-                <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+                <ReactMarkdown
+                    remarkPlugins={[remarkGfm]}
+                    components={{
+                        img: ({ src, alt }) => (
+                            <Image
+                                src={typeof src === 'string' ? src : ''}
+                                alt={alt || ''}
+                                width={800}
+                                height={600}
+                                style={{ width: '100%', height: 'auto' }}
+                            />
+                        )
+                    }}
+                >
+                    {markdown}
+                </ReactMarkdown>
             </div>
         </div>
     );
