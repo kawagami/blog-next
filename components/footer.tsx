cc297338272be5b93698ecba91bb0e7d182e1a0e
kawagami <joelai1988@gmail.com>
2026-06-16 00:37:01 +0800

    refactor: GitHub icon SVG 改 inline TSX component（currentColor 變色），刪 assets 圖檔

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,11 @@
-import Image from "next/image";
-import GithubMark from "@/assets/github-mark.svg";
-import GithubMarkWhite from "@/assets/github-mark-white.svg";
 import Link from "next/link";
+import GithubMark from "@/components/github-mark";
 
 export default function Footer() {
     return (
         <footer className="min-h-[50px] flex justify-center gap-4 text-center">
             <Link className="hover:scale-90" target="_blank" href="https://github.com/kawagami">
-                <Image src={GithubMark} width={30} height={30} alt="my github" className="dark:hidden" />
-                <Image src={GithubMarkWhite} width={30} height={30} alt="my github" className="hidden dark:block" />
+                <GithubMark className="text-neutral-900 dark:text-white" />
             </Link>
         </footer>
     );
